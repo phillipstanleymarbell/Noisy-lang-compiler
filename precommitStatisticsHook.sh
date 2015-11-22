bb5f9114d65534908d7cc2479e27e771dd9cbfa7
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2015-11-22 15:38:07 -0500

    Activating commit hooks...

--- precommitStatisticsHook.sh
+++ precommitStatisticsHook.sh
@@ -15,13 +15,13 @@ make -j
 make README.sloccount
 
 cat version.c >> $trackingDirectory/$statsFile
-echo '\n./noisy-darwin-EN -O0 Examples/hellowWorld -s' >> $trackingDirectory/$statsFile
-./noisy-darwin-EN -O0 Examples/hellowWorld -s >> $trackingDirectory/$statsFile
+echo '\n./noisy-darwin-EN -O0 Examples/helloWorld.n -s' >> $trackingDirectory/$statsFile
+./noisy-darwin-EN -O0 Examples/helloWorld.n -s >> $trackingDirectory/$statsFile
 
-echo '\nsudo dtrace -qs $dtraceDirectory/fcalls.d -c "./noisy-darwin-EN -O0 Examples/hellowWorld -s"' >> $trackingDirectory/$statsFile
-sudo dtrace -qs $dtraceDirectory/fcalls.d -c "./noisy-darwin-EN -O0 Examples/hellowWorld -s" noisy-darwin-EN >> $trackingDirectory/$statsFile
+echo '\nsudo dtrace -qs $dtraceDirectory/fcalls.d -c "./noisy-darwin-EN -O0 Examples/helloWorld.n -s"' >> $trackingDirectory/$statsFile
+sudo dtrace -qs $dtraceDirectory/fcalls.d -c "./noisy-darwin-EN -O0 Examples/helloWorld.n -s" noisy-darwin-EN >> $trackingDirectory/$statsFile
 
-echo '\nsudo dtrace -qs $dtraceDirectory/malloc.d -c "./noisy-darwin-EN -O0 Examples/hellowWorld -s"' >> $trackingDirectory/$statsFile
-sudo dtrace -qs $dtraceDirectory/malloc.d -c "./noisy-darwin-EN -O0 Examples/hellowWorld -s" >> $trackingDirectory/$statsFile
+echo '\nsudo dtrace -qs $dtraceDirectory/malloc.d -c "./noisy-darwin-EN -O0 Examples/helloWorld.n -s"' >> $trackingDirectory/$statsFile
+sudo dtrace -qs $dtraceDirectory/malloc.d -c "./noisy-darwin-EN -O0 Examples/helloWorld.n -s" >> $trackingDirectory/$statsFile
 
 cp $trackingDirectory/$statsFile $trackingDirectory/latest.txt
