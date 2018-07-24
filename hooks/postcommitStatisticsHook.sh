01c6aeaea40e1cf55806a113a106367631db8e25
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2018-07-24 08:47:09 +0100

    Tree cleanup

--- hooks/postcommitStatisticsHook.sh
+++ hooks/postcommitStatisticsHook.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-trackingDirectory=analysis/Statistics
+trackingDirectory=analysis/statistics
 
 #
 #	Since the pre-commit hook leads to a change of the hash, we saved the hash at the point of commit in the file '.noisy-last-head'
