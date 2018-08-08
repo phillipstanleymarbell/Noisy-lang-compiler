e5e68a669a74f4b6115ccca7cb04d5aa41ca2b8d
Phillip Stanley-Marbell <phillip.stanleymarbell@gmail.com>
2018-08-08 20:10:46 +0100

    Cleaned up hooks... -e Addresses #318

--- hooks/precommitStatisticsHook-linux.sh
+++ hooks/precommitStatisticsHook-linux.sh
@@ -20,7 +20,8 @@ make clean
 make -j
 make README.sloccount
 
-cat version.c >> $trackingDirectory/$statsFile
+cat src/noisy/version.c >> $trackingDirectory/$statsFile
+cat src/newton/version.c >> $trackingDirectory/$statsFile
 echo '\n./src/noisy/noisy-linux-EN -O0 applications/noisy/helloWorld.n -s' >> $trackingDirectory/$statsFile
 ./src/noisy/noisy-linux-EN -O0 applications/noisy/helloWorld.n -s >> $trackingDirectory/$statsFile
 echo '\n./src/newton/newton-linux-EN -S tmp.smt2 applications/newton/invariants/PendulumAcceleration.nt' >> $trackingDirectory/$statsFile
