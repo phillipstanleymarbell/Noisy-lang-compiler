ccba4aa7f4fd8067bb5d209875b21918288ff576
Kiseki Hirakawa <36139949+KisekiHirakawa@users.noreply.github.com>
2019-08-17 14:51:07 +0100

    Update common-irPass-helpers.h

--- src/common/common-irPass-helpers.h
+++ src/common/common-irPass-helpers.h
@@ -1,5 +1,6 @@
 /*
-	Authored 2015. Phillip Stanley-Marbell.
+	Authored 2015-2018, Phillip Stanley-Marbell. Modified 2017, Jonathan Lim.
+	Updated  2019, Kiseki Hirakawa.
 
 	All rights reserved.
 
@@ -21,21 +22,51 @@
 		derived from this software without specific prior written
 		permission.
 
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTorS
 	"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
 	FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-	COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+	COPYRIGHT OWNER OR CONTRIBUTorS BE LIABLE FOR ANY DIRECT, INDIRECT,
 	INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
 	BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-	LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+	LIABILITY, OR TorT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 	ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 	POSSIBILITY OF SUCH DAMAGE.
 */
 
-void			irPassHelperColorIr(State *  N, IrNode *  p, IrNodeColor nodeColor, bool setNotClear, bool recurseFlag);
-void			irPassHelperColorSymbolTable(State *  N, Scope *  p, IrNodeColor nodeColor, bool setNotClear, bool recurseFlag);
-uint64_t		irPassHelperIrSize(State *  N, IrNode *  p);
-uint64_t		irPassHelperSymbolTableSize(State *  N, Scope *  p);
+IrNode *	genIrNode(State *  N, IrNodeType type, IrNode *  irLeftChild, IrNode *  irRightChild, SourceInfo *  sourceInfo);
+IrNode *	shallowCopyIrNode(State *  N, IrNode *  original);
+void		errorUseBeforeDefinition(State *  N, const char *  identifier);
+void		errorMultiDefinition(State *  N, Symbol *  symbol);
+IrNode *	depthFirstWalk(State *  N, IrNode *  node);
+int 		countIrNodeOfType(State *  N, IrNode *  node, IrNodeType expectedType);
+IrNode *	depthFirstWalkNoLeftChild(State *  N, IrNode *  node);
+void		addLeaf(State *  N, IrNode *  parent, IrNode *  newNode);
+void		addLeafWithChainingSeq(State *  N, IrNode *  parent, IrNode *  newNode);
+void		addLeafWithChainingSeqNoLex(State *  N, IrNode *  parent, IrNode *  newNode, SourceInfo *  srcInfo);
+bool		peekCheck(State *  N, int lookAhead, IrNodeType expectedType);
+IrNode *	findNthIrNodeOfType(State *  N, IrNode *  root, IrNodeType expectedType, int nth);
+IrNode *	findNthIrNodeOfTypes(State *  N, IrNode *  root, IrNodeType productionOrToken, int firsts[kNoisyIrNodeTypeMax][kNoisyIrNodeTypeMax], int nth);
+IrNode *	findNthIrNodeOfTypeHelper(State *  N, IrNode *  root, IrNodeType expectedType, int *  nth);
+IrNode *	findNthIrNodeOfTypesHelper(State *  N, IrNode *  root, IrNodeType productionOrToken, int firsts[kNoisyIrNodeTypeMax][kNoisyIrNodeTypeMax], int *  nth);
+IrNodeType	getTypeFromOperatorSubtree(State *  N, IrNode *  n);
+
+#define L(node)		(node)->irLeftChild
+#define R(node)		(node)->irRightChild
+#define P(node)		(node)->irParent
+
+#define LL(node)	L(L(node))
+#define LLR(node)	R(L(L(node)))
+#define LR(node)	R(L(node))
+#define LRL(node)	L(R(L(node)))
+#define LRR(node)	R(R(L(node)))
+#define LRRL(node)	L(R(R(L(node))))
+#define RL(node)	L(R(node))
+#define RR(node)	R(R(node))
+#define RRL(node)	L(R(R(node)))
+#define RLL(node)	L(L(R(node)))
+#define RLLR(node)	R(L(L(R(node))))
+#define RLLLR(node)	R(L(L(L(R(node)))))
+#define RLR(node)	R(L(R(node)))
