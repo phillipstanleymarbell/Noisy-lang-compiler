f304aec11e9be25d48dd5f9b2d987dad50521838
Kiseki Hirakawa <36139949+KisekiHirakawa@users.noreply.github.com>
2019-08-17 14:36:28 +0100

    Update newton-irPass-dimensionalMatrixAnnotation.c
    Added functionality to get the list of identifiers from the invariant body

--- src/newton/newton-irPass-dimensionalMatrixAnnotation.c
+++ src/newton/newton-irPass-dimensionalMatrixAnnotation.c
@@ -59,6 +59,169 @@
 #include "common-irPass-helpers.h"
 #include "newton-types.h"
 #include "newton-symbolTable.h"
+#include "common-irHelpers.h"
+
+
+void
+irPassDimensionalMatrixAnnotationByBody(State *  N)
+{
+	Invariant *	invariant = N->invariantList;
+
+	while (invariant)
+	{
+		int		dimensionCount = 0;
+		IrNode *	parameterbody = invariant->constraints;
+		Dimension *	dimension = findNthIrNodeOfType(N, parameterbody, kNewtonIrNodeType_Tidentifier, 0)->physics->dimensions;
+		int		parameterCount = countIrNodeOfType(N, parameterbody, kNewtonIrNodeType_Tidentifier);
+	
+		/*
+		 *	First, count the number of parameters and dimensions
+		 */
+		Dimension * 	tempdimension = dimension;
+		
+		for ( ; tempdimension ; tempdimension = tempdimension->next)
+		{
+			dimensionCount++;
+		}
+
+		if (parameterCount == 0 || dimensionCount == 0)
+		{
+			fatal(N, Esanity);
+		}
+
+		/*
+		 *	Next, allocate a temporary matrix to hold the full list
+		 *	of parameter and dimension values.
+		 *
+		 *	We have a list of parameters, each of which has a list of
+		 *	_all_ the dimensions defined in a Newton description,
+		 *	including those that the parameter in question does not use.
+		 *
+		 *	We construct the temporary matrix with a row for each parameter,
+		 *	and a column for each of the (possibly unused) dimensions.
+		 *	Later, below, we determine which dimensions we actually use
+		 *	and recreate the _true_ dimensional matrix which has
+		 *	parameters in columns and dimensions in rows.
+		 */
+		double **	tmpMatrix = (double **)calloc(parameterCount, sizeof(double *));
+
+		if (!tmpMatrix)
+		{
+			fatal(N, Emalloc);
+		}
+
+		for (int i = 0; i < parameterCount; i++)
+		{
+			tmpMatrix[i] = (double *)calloc(dimensionCount, sizeof(double));
+			if (!tmpMatrix[i])
+			{
+				fatal(N, Emalloc);
+			}
+		}
+
+		invariant->dimensionalMatrixColumnLabels = (char **) calloc(parameterCount, sizeof(char *));
+
+		if (!invariant->dimensionalMatrixColumnLabels)
+		{
+			fatal(N, Emalloc);
+		}
+
+		/*
+		 *	Build the temporary matrix and populate the array usedDimensions[]
+		 *	with the indices of dimensions used by _any_ of the parameters:
+		 */
+		bool	usedDimensions[dimensionCount];
+		bzero(usedDimensions, sizeof(usedDimensions));
+
+		for (int i = 0; i < parameterCount; i++)
+		{
+			tempdimension = findNthIrNodeOfType(N,parameterbody,kNewtonIrNodeType_Tidentifier,i)->physics->dimensions;
+			invariant->dimensionalMatrixColumnLabels[i] = findNthIrNodeOfType(N,parameterbody,kNewtonIrNodeType_Tidentifier,i)->physics->identifier;
+			for (int j = 0; j < dimensionCount; j++)
+			{
+				tmpMatrix[i][j] = tempdimension->exponent;
+				if (tmpMatrix[i][j])
+				{
+					usedDimensions[j] |= 1;
+				}
+				tempdimension = tempdimension->next;
+			}
+		}
+
+		/*
+		 *	We already know the dimensional matrix column count, since
+		 *	that is the parameter count. We determine the row count
+		 *	from inspecting usedDimensions[].
+		 */
+		invariant->dimensionalMatrixColumnCount = parameterCount;
+		invariant->dimensionalMatrixRowCount = 0;
+		for (int i = 0; i < dimensionCount; i++)
+		{
+			if (usedDimensions[i])
+			{
+				invariant->dimensionalMatrixRowCount++;
+			}
+		}
+
+		if (invariant->dimensionalMatrixRowCount == 0)
+		{
+			fatal(N, Esanity);
+		}
+
+		invariant->dimensionalMatrixRowLabels = (char **) calloc(invariant->dimensionalMatrixRowCount, sizeof(char *));
+		if (!invariant->dimensionalMatrixRowLabels)
+		{
+			fatal(N, Emalloc);
+		}
+
+		/*
+		 *	Next, use usedDimensions[] to determine how many rows
+		 *	we need in the dimensional matrix. At the same time,
+		 *	we also collect the dimension string names:
+		 */
+		int	copiedRowLabelCount = 0;
+
+		tempdimension = dimension;
+		for (int i = 0; i < dimensionCount; i++)
+		{
+			if (usedDimensions[i])
+			{
+				invariant->dimensionalMatrixRowLabels[copiedRowLabelCount++] = tempdimension->name;
+			}
+			tempdimension = tempdimension->next;
+		}
+
+		/*
+		 *	Now, finally, allocate the dimensional matrix:
+		 */
+		invariant->dimensionalMatrix = (double *)calloc(invariant->dimensionalMatrixRowCount*invariant->dimensionalMatrixColumnCount, sizeof(double));
+		if (!invariant->dimensionalMatrix)
+		{
+			fatal(N, Emalloc);
+		}
+
+		/*
+		 *	Copy over the temporary matrix in the correct form
+		 *	(rows are dimensions, columns are parameters).
+		 */
+		int	copiedDimensionCount;
+		for (int i = 0; i < parameterCount; i++)
+		{
+			copiedDimensionCount = 0;
+			for (int j = 0; j < dimensionCount; j++)
+			{
+				if (usedDimensions[j])
+				{
+					invariant->dimensionalMatrix[invariant->dimensionalMatrixColumnCount*(copiedDimensionCount++) + i] = tmpMatrix[i][j];
+				}
+			}
+		}
+
+		free(tmpMatrix);
+
+		invariant = invariant->next;
+	}
+}
 
 
 void
@@ -85,8 +248,6 @@ irPassDimensionalMatrixAnnotation(State *  N)
 		{
 			dimensionCount++;
 		}
-
-//fprintf(stderr, "In irPassDimensionalMatrixAnnotation(), parameterCount=[%d], dimensionCount=[%d]", parameterCount, dimensionCount);
 
 		if (parameterCount == 0 || dimensionCount == 0)
 		{
@@ -135,7 +296,6 @@ irPassDimensionalMatrixAnnotation(State *  N)
 		bool	usedDimensions[dimensionCount];
 		bzero(usedDimensions, sizeof(usedDimensions));
 
-//fprintf(stderr, "In irPassDimensionalMatrixAnnotation():\n\n");
 		parameter = invariant->parameterList;
 		for (int i = 0; i < parameterCount; i++)
 		{
@@ -143,8 +303,6 @@ irPassDimensionalMatrixAnnotation(State *  N)
 			invariant->dimensionalMatrixColumnLabels[i] = parameter->irLeftChild->physics->identifier;
 			for (int j = 0; j < dimensionCount; j++)
 			{
-//fprintf(stderr, "\tparam %i, dim %d, dim mat col label %d = [%s], dim exponent = [%f]\n", i, j, i, invariant->dimensionalMatrixColumnLabels[i], dimension->exponent);
-
 				tmpMatrix[i][j] = dimension->exponent;
 				if (tmpMatrix[i][j])
 				{
