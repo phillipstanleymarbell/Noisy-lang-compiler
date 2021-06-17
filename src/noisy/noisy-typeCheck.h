0228a75136e9ca0291d5057892e6fd74bf0986fc
Angelos Plevris <angelospl48@gmail.com>
2021-06-17 18:00:28 +0300

    Move NoisyType to common data structures
    Addresses #534.

--- src/noisy/noisy-typeCheck.h
+++ src/noisy/noisy-typeCheck.h
@@ -1,45 +1,5 @@
 void noisyTypeCheck(State * N);
 bool noisySignatureIsMatching(State * N, IrNode * definitionSignature, IrNode * declarationSignature);
-
-
-typedef enum
-{
-        noisyInitType,
-        noisyBool,
-        noisyInt4,
-        noisyInt8,
-        noisyInt16,
-        noisyInt32,
-        noisyInt64,
-        noisyInt128,
-        noisyIntegerConstType,
-        noisyNat4,
-        noisyNat8,
-        noisyNat16,
-        noisyNat32,
-        noisyNat64,
-        noisyNat128,
-        noisyFloat16,
-        noisyFloat32,
-        noisyFloat64,
-        noisyFloat128,
-        noisyRealConstType,
-        noisyString,
-        noisyArrayType,
-        noisyArithType,
-        noisyNilType,
-        noisyTypeError
-} NoisyBasicType;
-
-typedef struct NoisyType
-{
-        NoisyBasicType basicType;
-        int dimensions;
-        NoisyBasicType arrayType;
-        int * sizeOfDimension;
-} NoisyType;
-
-
 NoisyType getNoisyTypeFromTypeExpr(State * N, IrNode * typeExpr);
 NoisyType getNoisyTypeFromBasicType(IrNode * basicType);
 void deallocateNoisyType(NoisyType * typ);
