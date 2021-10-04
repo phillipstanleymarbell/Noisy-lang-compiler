8145485002d01889d434a7ca7e8ad65112c99fce
Angelos Plevris <angelospl48@gmail.com>
2021-10-04 16:46:34 +0300

    Fix file extensions of files produced
    Addresses #534.

--- src/noisy/noisyCompileAndRunCoro.sh
+++ src/noisy/noisyCompileAndRunCoro.sh
@@ -13,7 +13,7 @@ name=$(basename $1 | cut -f 1 -d '.');
 
 ./noisy-`uname | tr '[:upper:]' '[:lower:]'`-EN $1;
 cd ../../applications/noisy;
-opt < $name.bc -O1 -enable-coroutines -S > $name-opt.bc;
-llc -filetype=obj $name-opt.bc -o $name.o;
+opt < $name.bc -O1 -enable-coroutines -S > $name-opt.ll;
+llc -filetype=obj $name-opt.ll -o $name.o;
 clang $name.o noisyLib.o -o $name;
 ./$name;
