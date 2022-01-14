6a41e29d6c2a9247f0ba6cf4d7ccb3561011c053
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-01-14 13:35:03 +0000

    Applied coding conventions test file.
    Addresses #550.

--- applications/newton/llvm-ir/c-files/application.c
+++ applications/newton/llvm-ir/c-files/application.c
@@ -9,8 +9,8 @@ typedef double signalAccelerationY;
 typedef double signalAccelerationZ;
 
 typedef struct complex {
-    signalAccelerationX x;
-    signalAccelerationY y;
+    signalAccelerationX	x;
+    signalAccelerationY	y;
 } complex;
 
 int
@@ -18,24 +18,32 @@ main (void)
 {
 	signalAccelerationX		accelerationX = 5.0;
 	signalAccelerationY		accelerationY = 5.0;
-    signalAccelerationX*	accelerationX_ptr = &accelerationX;
+    signalAccelerationX *	accelerationX_ptr = &accelerationX;
     double x;
 
     complex c;
-    c.x = accelerationY;
+    c.x = accelerationX;
     c.y = accelerationY;
 
 	/*
-	 * Dimensionally consistent statement
+	 * Dimensionally consistent statements
 	 */
 	*accelerationX_ptr = accelerationX + accelerationX;
+	x = accelerationX;
 
 	/*
-	 * Dimensionally inconsistent statement
+	 * Dimensionally inconsistent statements
+	 *
+	 * accelerationX = accelerationX || accelerationY;
+	 * accelerationX2 = accelerationX + accelerationY;
+	 *
+	 * Since x has already been assigned previously with a signalAccelerationX signal,
+	 * the following instruction is invalid:
+	 *
+	 * x = accelerationY;
+	 *
+	 * c.y = accelerationX;
 	 */
-//	*accelerationX2 = accelerationX + accelerationY;
-//    x = accelerationX;
-//    x = accelerationY;
 
     printf("%f\n", accelerationX);
 	return 0;
