2ed4676fadc4e25f48a265d7cf3887accde6cdb8
Orestis Kaparounakis <orestis.kapar@hotmail.com>
2022-03-17 16:34:32 +0200

    Add state estimator dimensionality check testcase
    Addresses #583.

---
+++ applications/newton/llvm-ir/state-estimator/RandomAccelerationWalk-estSynth.h
@@ -0,0 +1,33 @@
+/*
+ *	Generated .h file from Newton file: /home/koma/Noisy-lang-compiler/applications/newton/invariants/RandomAccelerationWalk-estSynth.nt
+ */
+
+typedef double time;
+typedef double distance;
+typedef double mass;
+typedef double material;
+typedef double temperature;
+typedef double charge;
+typedef double current;
+typedef double angle;
+typedef double speed;
+typedef double acceleration;
+typedef double force;
+typedef double work;
+typedef double power;
+typedef double magneticFluxDensity;
+typedef double angularRate;
+typedef double frequency;
+typedef double area;
+typedef double density;
+typedef double volume;
+typedef double angularDisplacement;
+typedef double angularVelocity;
+typedef double pressure;
+typedef double concentration;
+typedef double relativeHumidity;
+
+/*
+ *	End of the generated .h file
+ */
+
