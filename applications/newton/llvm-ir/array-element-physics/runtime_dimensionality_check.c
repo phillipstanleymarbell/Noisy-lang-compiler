d18786e2d39f0e27830b39cba1d74b3a51211c50
Nikos Mavrogeorgis <mavrogeorgis.nikos@gmail.com>
2022-04-15 19:39:53 +0100

    Added simple runtime check function for dimensionality check.
    Addresses #586.

---
+++ applications/newton/llvm-ir/array-element-physics/runtime_dimensionality_check.c
@@ -0,0 +1,11 @@
+//
+// Created by blackgeorge on 15/4/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void __array_dimensionality_check(int64_t index)
+{
+	printf("Found index: %ld\n", index);
+}
