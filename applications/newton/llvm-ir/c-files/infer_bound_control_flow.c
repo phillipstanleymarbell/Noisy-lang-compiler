95fee3adbfda9fae695635fc15705c4f98b10e02
ds1231h <ds1231h@126.com>
2022-05-11 17:41:55 +0800

    add basic implement with simplifyControlFlow
    Addresses #594.

--- applications/newton/llvm-ir/c-files/infer_bound_control_flow.c
+++ applications/newton/llvm-ir/c-files/infer_bound_control_flow.c
@@ -65,6 +65,9 @@ main (void)
 	}
 
 	// increasing
+	// if x in [-16, 2], this for block should be eliminated
+	// if x in [3, 16], y6=y6+(x-i0)
+	// so the range of y6 is [y6, y6+x-2], where x>2
 	for (int i = 2; i < x; i++)
 	{
 		// it's better to infer the range of y6, which is [6, 20]
@@ -72,6 +75,9 @@ main (void)
 	}
 
 	// decreasing
+	// if x in [1, 16], this for block should be eliminated
+	// if x in [-16, 0], y7 = y7+(i0-x)
+	// so the range of y7 is [y7, y7+1-x], where x<1
 	for (int i = 1; i > x; i--)
 	{
 		// it's better to infer the range of y7, which is [7, 24]
@@ -92,6 +98,9 @@ main (void)
 	}
 
 	// increasing
+	// if x in [5, 16], this for block should be eliminated
+	// if x in [-16, 4], y10=y10+(5-x)
+	// so the range of y6 is [y10, y10+5-x], where x<5
 	while (x < 5)
 	{
 		// it's better to infer the range of y10, which is [10, 31]
@@ -100,6 +109,9 @@ main (void)
 	}
 
 	// decreasing
+	// if x in [-16, 0], this for block should be eliminated
+	// if x in [1, 16], y11=y11+(x-0)
+	// so the range of y11 is [y11, y11+x-0], where x>0
 	while (x > 0)
 	{
 		// it's better to infer the range of y11, which is [11, 27]
--- src/newton/newton-irPass-LLVMIR-simplify-control-flow-by-range.cpp
+++ src/newton/newton-irPass-LLVMIR-simplify-control-flow-by-range.cpp
@@ -51,7 +51,108 @@ typedef struct BoundInfo {
 void
 simplifyControlFlow(State * N, BoundInfo * boundInfo, Function & llvmIrFunction)
 {
+	std::map<Value *, std::pair<double, double>> virtualRegisterRange;
+	for (BasicBlock & llvmIrBasicBlock : llvmIrFunction) {
+		for (Instruction &llvmIrInstruction : llvmIrBasicBlock) {
+			switch (llvmIrInstruction.getOpcode()) {
+				case Instruction::Call:
+					if (auto llvmIrCallInstruction = dyn_cast<CallInst>(&llvmIrInstruction)) {
+						Function *calledFunction = llvmIrCallInstruction->getCalledFunction();
+						if (calledFunction->getName().startswith("llvm.dbg.declare")) {
+							auto firstOperator = cast<MetadataAsValue>(llvmIrCallInstruction->getOperand(0));
+							auto localVariableAddressAsMetadata = cast<ValueAsMetadata>(firstOperator->getMetadata());
+							auto localVariableAddress = localVariableAddressAsMetadata->getValue();
 
+							auto variableMetadata = cast<MetadataAsValue>(llvmIrCallInstruction->getOperand(1));
+							auto debugInfoVariable = cast<DIVariable>(variableMetadata->getMetadata());
+							auto variableType = debugInfoVariable->getType();
+
+							// if we find such type in boundInfo->typeRange,
+							// we get its range and bind the var with it in boundInfo->variableBound
+							// and record it in the virtualRegisterRange
+							auto typeRangeIt = boundInfo->typeRange.find(variableType->getName().str());
+							if (typeRangeIt != boundInfo->typeRange.end()) {
+								boundInfo->variableBound.emplace(debugInfoVariable->getName().str(), typeRangeIt->second);
+								virtualRegisterRange.emplace(localVariableAddress, typeRangeIt->second);
+							}
+						}
+					}
+					break;
+
+				case Instruction::Load:
+					if (auto llvmIrLoadInstruction = dyn_cast<LoadInst>(&llvmIrInstruction))
+					{
+						auto vrRangeIt = virtualRegisterRange.find(llvmIrLoadInstruction->getOperand(0));
+						if (vrRangeIt != virtualRegisterRange.end())
+						{
+							virtualRegisterRange.emplace(llvmIrLoadInstruction, vrRangeIt->second);
+						}
+					}
+					break;
+				case Instruction::ICmp:
+				case Instruction::FCmp: {
+					auto leftOperand = llvmIrInstruction.getOperand(0);
+					auto rightOperand = llvmIrInstruction.getOperand(1);
+					/// todo: expression normalization needed, which simpily the "const cmp const" or normalize into the "var cmp const" form
+					/// so this if-branch is a debug message, and will be deleted after finishing the expression normalization
+					if ((isa<llvm::Constant>(leftOperand) && isa<llvm::Constant>(rightOperand)) ||
+							(isa<llvm::Constant>(leftOperand) && !isa<llvm::Constant>(rightOperand)))
+					{
+						flexprint(N->Fe, N->Fm, N->Fperr, "\tExpression normalization needed.\n");
+					}
+
+					if (!isa<llvm::Constant>(leftOperand) && !isa<llvm::Constant>(rightOperand))
+					{
+
+					}
+					else if (!isa<llvm::Constant>(leftOperand) && isa<llvm::Constant>(rightOperand))
+					{
+						// eg. fcmp ogt double %1, 0
+						// get the constant value
+						double constValue = 0.0;
+						if (ConstantFP * constFp = llvm::dyn_cast<llvm::ConstantFP>(rightOperand))
+						{
+							// both "float" and "double" type can use "convertToDouble"
+							constValue = (constFp->getValueAPF()).convertToDouble();
+						}
+						flexprint(N->Fe, N->Fm, N->Fpinfo, "\tCmp: right operand: %f\n", constValue);
+						// find the variable from the virtualRegisterRange
+						auto vrRangeIt = virtualRegisterRange.find(leftOperand);
+						if (vrRangeIt != virtualRegisterRange.end())
+						{
+
+						}
+					}
+					break;
+				}
+
+				case Instruction::Br:
+					if (auto llvmIrBrInstruction = dyn_cast<BranchInst>(&llvmIrInstruction))
+					{
+						flexprint(N->Fe, N->Fm, N->Fpinfo, "\tInstruction::Br\n");
+						if (llvmIrBrInstruction->isConditional())
+						{
+							flexprint(N->Fe, N->Fm, N->Fpinfo, "\tconditional\n");
+							auto condValue = llvmIrBrInstruction->getCondition();
+							auto vrRangeIt = virtualRegisterRange.find(condValue);
+							if (vrRangeIt != virtualRegisterRange.end())
+							{
+								// if true, remove the condition
+								// if false, remove the whole block
+							}
+						}
+						else
+						{
+							flexprint(N->Fe, N->Fm, N->Fpinfo, "\tunconditional\n");
+						}
+					}
+					break;
+
+				default:
+					continue;
+			}
+		}
+	}
 }
 
 void irPassLLVMIRSimplifyControlFlowByRange(State *N) {
@@ -92,6 +193,9 @@ void irPassLLVMIRSimplifyControlFlowByRange(State *N) {
 	{
 		simplifyControlFlow(N, boundInfo, mi);
 	}
+
+	// todo: dump the output IR
+
 }
 
 }
