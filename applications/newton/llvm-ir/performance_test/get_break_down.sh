45f8c715d7ebc7c4185568ec8fcf50fda6686bda
Pei Mu <ds1231h@126.com>
2023-03-24 15:01:46 +0000

    use 0-200
    Addresses #647.

--- applications/newton/llvm-ir/performance_test/get_break_down.sh
+++ applications/newton/llvm-ir/performance_test/get_break_down.sh
@@ -11,7 +11,7 @@ fi
 make perf_float64_add
 echo "without CoSense" >> "$file"
 for i in {1..100}; do
-  ./main_out 35.75 135.33 >> "$file"
+  ./main_out 0 200 >> "$file"
 done
 wc -l out.ll >> "$file"
 
@@ -20,7 +20,7 @@ cp "$PWD"/../../../../src/newton/newton-linux-EN_v0_1 "$PWD"/../../../../src/new
 make perf_float64_add_opt
 echo "with type compression" >> "$file"
 for i in {1..100}; do
-  ./main_out 35.75 135.33 >> "$file"
+  ./main_out 0 200 >> "$file"
 done
 wc -l out.ll >> "$file"
 
@@ -28,7 +28,7 @@ cp "$PWD"/../../../../src/newton/newton-linux-EN_v0_2 "$PWD"/../../../../src/new
 make perf_float64_add_opt
 echo "with branch elimination" >> "$file"
 for i in {1..100}; do
-  ./main_out 35.75 135.33 >> "$file"
+  ./main_out 0 200 >> "$file"
 done
 wc -l out.ll >> "$file"
 
@@ -36,7 +36,7 @@ cp "$PWD"/../../../../src/newton/newton-linux-EN_v0_3 "$PWD"/../../../../src/new
 make perf_float64_add_opt
 echo "with const substitution" >> "$file"
 for i in {1..100}; do
-  ./main_out 35.75 135.33 >> "$file"
+  ./main_out 0 200 >> "$file"
 done
 wc -l out.ll >> "$file"
 
@@ -44,6 +44,6 @@ cp "$PWD"/../../../../src/newton/newton-linux-EN_v0 "$PWD"/../../../../src/newto
 make perf_float64_add_opt
 echo "overall" >> "$file"
 for i in {1..100}; do
-  ./main_out 35.75 135.33 >> "$file"
+  ./main_out 0 200 >> "$file"
 done
 wc -l out.ll >> "$file"
